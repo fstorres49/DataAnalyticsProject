1fce26b21331aeda8a57a1c3de030138131076b5
Franco Sanchez <fsanchez@actualize.ca>
2021-11-14 20:40:56 +0100

    Adding code

--- app/src/store/data.js
+++ app/src/store/data.js
@@ -1,6 +1,6 @@
 export const namespaced = true;
 export const state = {
-    title: "Can an average american buy a house in 2021?"
+    title: "Can an average american buy a house in 2021 ?"
 }
 export const mutations = {}
 export const actions = {}
